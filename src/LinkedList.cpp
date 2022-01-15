a64945b63031a05e18a6b98cef43ef83fcf8d77e
mohamedtamer0 <mt4452242@gmail.com>
2022-01-15 17:27:41 +0200

    update

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -1,6 +1,45 @@
-//
-// Created by Alwqar on 1/15/2022.
-//
+#include <iostream>
+
+using namespace std;
+
+
+class Node {
+public:
+    int data;
+    Node *next;
+};
+
+class Linkedlist {
+public:
+    Node *head;
+
+    Linkedlist() {
+        head = NULL;
+    }
+
+    bool isempty() {
+        return (head == NULL);
+    }
+
+
+    void InsertFirst(int newvalue) {
+        Node *newnode;
+        newnode->data = newvalue;
+        if (isempty()) {
+            newnode->next = NULL;
+            head = newnode;
+        } else {
+            newnode->next = head;
+            head = newnode;
+        }
+    }
+
+
+
+
+
+};
+
 
 int main() {
 
