be8d311eaedc19dd134accc5c045ca231ac64e4a
mohamedtamer0 <mt4452242@gmail.com>
2022-01-17 05:50:53 +0200

    update

--- src/BinarySearchTree.cpp
+++ src/BinarySearchTree.cpp
@@ -71,5 +71,24 @@ public:
 };
 
 int main() {
+    // 45,15,79,90,10,55,12,20,50
+    BST btree;
+    btree.Insert(45);
+    btree.Insert(15);
+    btree.Insert(79);
+    btree.Insert(90);
+    btree.Insert(10);
+    btree.Insert(55);
+    btree.Insert(12);
+    btree.Insert(20);
+    btree.Insert(50);
+
+    cout << "Display the Tree Content \n";
+    btree.Preorder(btree.root);
+    cout << "\n..............................................\n";
+    btree.Inorder(btree.root);
+    cout << "\n..............................................\n";
+    btree.Postorder(btree.root);
+    cout << "\n..............................................\n";
 
 }
