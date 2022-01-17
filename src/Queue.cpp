ee960e1a770b271989e7ef96f150e06dc8232b55
mohamedtamer0 <mt4452242@gmail.com>
2022-01-17 03:15:20 +0200

    update

--- src/Queue.cpp
+++ src/Queue.cpp
@@ -1,4 +1,46 @@
+#include <iostream>
 
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+};
+
+
+class Queue {
+public:
+    Node *front;
+    Node *rear;
+
+    Queue() {
+        front = rear = NULL;
+    }
+
+    bool isEmpty() {
+        return (front == NULL && rear == NULL);
+    }
+
+
+    void Enqueue(int item) {
+        Node *newnode = new Node();
+        newnode->data = item;
+
+        if (isEmpty()) {
+            front = rear = newnode;
+        } else {
+            rear->next = newnode;
+        }
+    }
+
+
+};
 
 
 int main() {
