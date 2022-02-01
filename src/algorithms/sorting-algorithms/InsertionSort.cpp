7c877f718837e8d29dde0b8b3eadd285c8180a27
mohamedtamer0 <mt4452242@gmail.com>
2022-02-01 04:15:54 +0200

    update

---
+++ src/algorithms/sorting-algorithms/InsertionSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    int i, j, key;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+/* Function to print an array */
+void printArray(int arr[], int size) {
+    for (int j = 0; j < size; j++) {
+        cout << arr[j] << " ";
+    }
+    cout << endl;
+
+}
+
+int main() {
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
